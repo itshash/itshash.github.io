4ef3c115b3a8fcc22f5da87ceec26907da38b42e
Pedro <hshdev@gmail.com>
2016-08-23 11:18:17 -0300

    adicionado nav menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,7 @@
 console.log('This would be the main JS file.');
+
+$("#a-sec-bio").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#sec-bio").offset().top
+    }, 2000);
+});
